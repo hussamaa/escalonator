420415273762f9a1c2a934142cc28335f18f996e
Hussama Ismail <hussamaismail@gmail.com>
2013-07-26 21:06:14 -0400

    Implementacao de uma classe utilitaria com metodo que ordena as tarefas do escalonador com base no deadline e no tempo de computacao

--- source/scheduler/src/main/java/br/eti/hussamaismail/scheduler/domain/StaticScheduler.java
+++ source/scheduler/src/main/java/br/eti/hussamaismail/scheduler/domain/StaticScheduler.java
@@ -1,51 +1,60 @@
 package br.eti.hussamaismail.scheduler.domain;
 
-import java.util.Collection;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 
 public class StaticScheduler implements Scheduler {
 
-	private Collection<PeriodicTask> tasks;
+	private List<PeriodicTask> tasks;
 	private Logger log = Logger.getLogger(StaticScheduler.class);
 
-	public Collection<PeriodicTask> getTasks() {
+	public List<PeriodicTask> getTasks() {
 		return tasks;
 	}
 
-	public void setTasks(Collection<PeriodicTask> tasks) {
+	public void setTasks(List<PeriodicTask> tasks) {
 		this.tasks = tasks;
 	}
 
 	/**
-	 * Metodo que realiza o teste de escalonabilidade
-	 * para as tarefas periodicas adicionadas ao escalonador.
+	 * Metodo que realiza o teste de escalonabilidade para as tarefas periodicas
+	 * adicionadas ao escalonador.
 	 * 
-	 * Este metodo por padrao o teste do RateMonotonic que consiste
-	 * na formula matematica:
+	 * Este metodo por padrao o teste do RateMonotonic que consiste na formula
+	 * matematica:
 	 * 
-	 * P1 = SUM(C/T) 
-	 * P2 = N * (2^(1/N) - 1)
+	 * P1 = SUM(C/T) P2 = N * (2^(1/N) - 1)
 	 * 
 	 * P1 <= P2
 	 * 
 	 * @return
 	 */
-	public Boolean isScalable(){
-	
+	public Boolean isScalable() {
+
 		log.debug("Inicializando teste de escalonabilidade");
-		
+
 		double p1 = 0, p2;
-		
-		for (PeriodicTask pt : this.getTasks()){
+
+		for (PeriodicTask pt : this.getTasks()) {
 			p1 = p1 + (pt.getComputationTime() / pt.getPeriod());
 		}
-		
-		p2 = this.getTasks().size() * (Math.pow(2, (double) 1/this.getTasks().size()) - 1);
 
-		log.debug("P1: " + p1 + " | P2: "+p2);
-	
+		p2 = this.getTasks().size()
+				* (Math.pow(2, (double) 1 / this.getTasks().size()) - 1);
+
+		log.debug("P1: " + p1 + " | P2: " + p2);
+
 		return p1 <= p2 ? true : false;
 	}
 	
+	/**
+	 * Metodo que calcula o tempo maximo de resposta para 
+	 * cada tarefa presente no escalonador estatico
+	 * 
+	 */
+	public void calculateMaximumResponseTimeToTheTasks(){
+
+	
+	}
 }
---
+++ source/scheduler/src/main/java/br/eti/hussamaismail/scheduler/util/TasksUtil.java
@@ -0,0 +1,70 @@
+package br.eti.hussamaismail.scheduler.util;
+
+import java.util.Collections;
+import java.util.Comparator;
+
+import br.eti.hussamaismail.scheduler.domain.PeriodicTask;
+import br.eti.hussamaismail.scheduler.domain.Scheduler;
+import br.eti.hussamaismail.scheduler.domain.StaticScheduler;
+
+
+public class TasksUtil {
+
+	/**
+	 * Metodo que ordena a lista de tarefas de um  
+	 * scheduler com base no seu deadline
+	 * 
+	 * @param scheduler
+	 */
+	public void sortTasksByDeadline(Scheduler scheduler){		
+		
+		if (!(scheduler instanceof StaticScheduler))
+			return;
+		
+		StaticScheduler staticScheduler = (StaticScheduler) scheduler; 
+		Collections.sort(staticScheduler.getTasks(), new Comparator<PeriodicTask>() {
+			public int compare(PeriodicTask pt1, PeriodicTask pt2) {
+				return compareValues(pt1.getDeadline(), pt2.getDeadline());
+			}
+		});		
+	}
+	
+	/**
+	 * Metodo que ordena a lista de tarefas de um  
+	 * scheduler com base no seu tempo de computacao
+	 * 
+	 * @param scheduler
+	 */
+	public void sortTasksByComputationTime(Scheduler scheduler){		
+		
+		if (!(scheduler instanceof StaticScheduler))
+			return;
+		
+		StaticScheduler staticScheduler = (StaticScheduler) scheduler; 
+		Collections.sort(staticScheduler.getTasks(), new Comparator<PeriodicTask>() {
+			public int compare(PeriodicTask pt1, PeriodicTask pt2) {
+				return compareValues(pt1.getComputationTime(), pt1.getComputationTime());
+			}
+		});		
+	}
+		
+	/**
+	 * Metodo que compara dois valores e devolve
+	 * o valor 1 caso o value1 seja maior que o value2,
+	 * devolve o valor -1 caso value1 seja menor que value2 e 
+	 * retorna 0 caso os dois sejam iguais
+	 * 
+	 * @param value1
+	 * @param value2
+	 * @return
+	 */
+	private int compareValues(double value1, double value2){
+		if (value1 > value2){
+			return 1;
+		}else if (value1 < value2){
+			return -1;
+		}else{
+			return 0;
+		}
+	}
+}
